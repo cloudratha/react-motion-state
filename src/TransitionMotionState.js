468001042d2ad6dbf1c472583212964d66ff2f77
cloudratha <3967435+cloudratha@users.noreply.github.com>
2018-10-07 16:10:20 +0100

    Replace setTimeout with Raf

--- src/TransitionMotionState.js
+++ src/TransitionMotionState.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TransitionMotion } from 'react-motion';
+import raf from 'raf';
 
 class TransitionMotionState extends React.Component {
   constructor(props) {
@@ -10,22 +11,27 @@ class TransitionMotionState extends React.Component {
       return accum;
     }, {});
 
-    this.styleTimeouts = {};
-
-    this.interpolateStyles = this.interpolateStyles.bind(this);
-    this.willEnter = this.willEnter.bind(this);
-    this.willLeave = this.willLeave.bind(this);
-    this.getState = this.getState.bind(this);
+    this.styleRafs = {};
   }
 
-  willEnter(config) {
+  countdown(key, count = 0) {
+    this.styleRafs[key] = raf(() => {
+      if (count >= 2) {
+        this.setState({ [key]: `${this.state[key]}-done` });
+      } else {
+        this.countdown(key, count + 1);
+      }
+    })
+  }
+
+  willEnter = (config) => {
     const state = this.props.willEnter ? 'enter' : 'enter-done';
     this.setState({ [config.key]: state });
 
     return this.props.willEnter ? this.props.willEnter(config) : config.style;
   }
 
-  willLeave(config) {
+  willLeave = (config) => {
     const state = this.props.willLeave ? 'exit' : 'exit-done';
     // willLeave will be called on each step to re-evaluate styles
     // So make sure we only update state when necessary
@@ -36,26 +42,26 @@ class TransitionMotionState extends React.Component {
     return this.props.willLeave ? this.props.willLeave(config) : config.style;
   }
 
-  getState(config) {
-    // Everytime the state is called lets attach a timeout to know when its done
-    clearTimeout(this.styleTimeouts[config.key]);
+  getState = (config) => {
+    // Everytime the state is called lets clear the previous countdown
+    if (this.styleRafs[config.key]) {
+      raf.cancel(this.styleRafs[config.key]);
+    }
     if (
       this.state[config.key] &&
       this.state[config.key].indexOf('done') === -1
     ) {
-      this.styleTimeouts[config.key] = setTimeout(() => {
-        this.setState({ [config.key]: `${this.state[config.key]}-done` });
-      }, 100);
+      this.countdown(config.key);
     }
     return this.state[config.key];
   }
 
-  interpolateStyles(styles) {
+  interpolateStyles = (styles) => {
     const stateStyles = styles.map(config => ({
       ...config,
       data: {
         ...config.data,
-        state: this.getState(config)
+        transition: this.getState(config)
       }
     }));
     return this.props.children(stateStyles);
